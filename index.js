b0ed4eb2ee43e7519f236190d06f0b871c90be87
xyieko <emmanuel.ontweka@student.moringaschool.com>
2025-03-24 15:20:06 +0300

    index.js and package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// Initialize employee object
+const employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St'
+  };
+  
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
